e594d05a0b7cffe255da8fba6b246ca5cb742ca7
Matthew Huskey <30731520+mhuskey@users.noreply.github.com>
2020-07-27 15:57:48 -0400

    Public Footer & Index
    • Added content for `private/shared/public_footer.php`
    • Added a hero image and the public footer to `public/index.php`
    • Added more CSS styling

--- public/index.php
+++ public/index.php
@@ -12,8 +12,14 @@
                   <a href="<?php echo url_for('/furniture_inventory.php'); ?>"><button type="button" class="btn btn-outline-primary">View Our Inventory</button></a>
                   <a href="<?php echo url_for('/about.php'); ?>"><button type="button" class="btn btn-outline-primary">About</button></a>
               </div>
+              
+              <div class="col-sm-10 offset-sm-1 text-center align-middle">
+                <img src="<?php echo url_for('/assets/images/hero.jpg'); ?>" class="img-fluid rounded" alt="Hero Image">
+              </div>
             </div>
           </div>
         </div>
       </section>
     </main>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
